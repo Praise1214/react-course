9f3a3f55dbcbc14b5ca2aa60d87039e739eb3057
Praise1214 <agapepraiz@gmail.com>
2025-06-13 15:43:14 +0100

    use data from the backend to generate the HTML

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,14 +1,19 @@
 import axios from 'axios'
+import { useEffect, useState} from 'react';
 import { Header } from '../components/Header'
 import CheckmarkIcon from '../assets/images/icons/checkmark.png'
-import { products } from '../data/products';
 import './Homepage.css'
 
 export function Homepage() {
 
-	axios.get('http://localhost:3000/api/products').then((response) => {
-		console.log(response.data)
-		})
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		axios.get('http://localhost:3000/api/products').then((response) => {
+			setProducts(response.data)
+			})
+	}, [])
+	
 	return (
 		<>
 			<link rel="icon" type="image/svg+xml" href="home-favicon.png" />
