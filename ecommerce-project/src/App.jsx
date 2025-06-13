14b277214be0467599b3b705a6ba40adf2a22ff2
Praise1214 <agapepraiz@gmail.com>
2025-06-13 17:06:34 +0100

    lift cart up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import {Routes, Route} from 'react-router';
 import { Homepage } from './pages/HomePage';
 import {CheckoutPage} from './pages/checkout/CheckoutPage';
@@ -7,10 +9,18 @@ import './App.css'
 import { TrackingPage } from './pages/TrackingPage';
 
 function App() {
+
+  const [cart, setCart] = useState([]);
+
+  useEffect (() => {
+    axios.get('/api/cart-items').then((response) => {
+			setCart(response.data)
+		})
+  }, [])
   return (
     <Routes>
-      <Route index element = {<Homepage />} />
-      <Route path= "checkout" element = {<CheckoutPage />} />
+      <Route index element = {<Homepage cart = {cart}/>} />
+      <Route path= "checkout" element = {<CheckoutPage cart = {cart}/>} />
       <Route path="orders" element = {<OrderPage />} />
       <Route path = "tracking" element = {<TrackingPage />}/>
       <Route path = "*" element = {<ErrorPage />}/>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,19 +4,15 @@ import { Header } from '../components/Header'
 import CheckmarkIcon from '../assets/images/icons/checkmark.png'
 import './Homepage.css'
 
-export function Homepage() {
+export function Homepage({cart}) {
 
 	const [products, setProducts] = useState([]);
-	const [cart, setCart] = useState([])
+	
  
 	useEffect(() => {
 		axios.get('/api/products').then((response) => {
 			setProducts(response.data)
 			})
-
-		axios.get('/api/cart-items').then((response) => {
-			setCart(response.data)
-		})
 			
 	}, [])
 	
